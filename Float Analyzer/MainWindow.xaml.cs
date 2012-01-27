1c0d4c158577dfb859ca38629dabdc1f19937ab2
Nathan Hartman <nathanial.hartman@gmail.com>
2012-01-26 22:40:45 -0600

    first commit

---
+++ Float Analyzer/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using MessageBox = Microsoft.Windows.Controls.MessageBox;
+
+namespace Float_Analyzer {
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+            if (DesignerProperties.GetIsInDesignMode(this)) return;
+
+
+        }
+
+        void CalculateClick(object sender, RoutedEventArgs e) {
+        }
+
+        void AboutClick(object sender, RoutedEventArgs e) {
+            MessageBox.Show("Erdos Miller Float Analyzer: 1.0", "Version", MessageBoxButton.OK,
+                            MessageBoxImage.Information);
+        }
+
+        void ExitClick(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
